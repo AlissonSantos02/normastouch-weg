3c3b107fc4906ac30bb98fa8cf7727908577cfcd
Arthur Hoengen <ahoengen@weg.net>
2025-11-27 18:01:54 +0000

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,12 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   build: { outDir: 'build', },
   server: {
-    host: "::",
     port: 3000,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 }));
