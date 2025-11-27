66bec228e47d751de8215de3c2d7b05c89e44a66
AlissonSantos02 <137784989+AlissonSantos02@users.noreply.github.com>
2025-11-27 14:51:18 -0300

    teste412

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: { outDir: 'build', },
   server: {
     host: "::",
     port: 8080,
